86a51bc6126dc7c67777cde98904656cf414e165
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2018-03-17 11:41:15 -0700

    refactoring

--- controllers/randomController.js
+++ controllers/randomController.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
+// generate random integer between min and max value
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
 exports.getRandomNum = (req, res) => {
-  axios.get('https://www.random.org/integers/?num=10000&min=0&max=1000&col=1&base=10&format=plain&rnd=new')
+  axios.get('https://www.random.org/integers/?num=10000&min=0&max=255&col=1&base=10&format=plain&rnd=new')
   .then((result) => {
     console.log(result.data);
     result = result.data.toString().split('\n');
@@ -15,13 +16,14 @@ exports.getRandomNum = (req, res) => {
     const height = 128;
     var grid = new Uint8ClampedArray(width * height * 4);
 
+    // generate grid for bitmap image from the random integers
     for(let i = 0 ; i < (width*height*4) ; i += 4) {
-      grid[i] = result[i%10000] % 255
-      grid[i+1] = Math.floor(result[i%10000] / getRandomArbitrary(2, 5)) % 255
-      grid[i+2] = Math.floor(result[i%10000] / getRandomArbitrary(2, 5)) % 255
-      grid[i+3] = Math.floor(result[i%10000] / getRandomArbitrary(2, 5)) % 255
+      grid[i] = result[i%10000];
+      grid[i+1] = Math.floor(result[i%10000] / getRandomArbitrary(2, 5));
+      grid[i+2] = Math.floor(result[i%10000] / getRandomArbitrary(2, 6));
+      grid[i+3] = Math.floor(result[i%10000] / getRandomArbitrary(2, 7));
     }
-
+    
     res.render('home', {
       grid
     });
